156f4dd775f095dc5ef515cc8fe35de87a674db2
uttamSarkar <103212353+uttam1235@users.noreply.github.com>
2022-12-18 20:02:02 +0530

    my NewsApp website

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import "./App.css";
+
+import React, { useState} from "react";
+import NavBar from "./Components/NavBar";
+import News from "./Components/News";
+import { BrowserRouter as Router,  Route,Routes } from "react-router-dom";
+import LoadingBar from 'react-top-loading-bar'
+const App =()=> {
+
+ const  pageSize=5;
+  const apiKey = process.env.REACT_APP_NEWS_API
+  
+  const [progress, setProgress] = useState(0)
+  
+
+    return (
+      <div>
+        <Router>
+        <NavBar/> 
+
+        <LoadingBar
+        color='#f11946'
+        progress={progress}
+      />
+          <Routes>
+          <Route exact path="/"element={<News setProgress={setProgress}  apiKey ={apiKey} key="general" pageSize={pageSize} country="in" category="general"/>}/>
+          <Route exact path="/business"element={<News setProgress={setProgress}  apiKey ={apiKey} key="business" pageSize={pageSize} country="in" category="business"/>}/> 
+          <Route exact path="/entertainment" element={<News setProgress={setProgress}  apiKey ={apiKey} key="entertainment" pageSize={pageSize} country="in" category="entertainment"/>}/> 
+          <Route exact path="/general"element={<News setProgress={setProgress}  apiKey ={apiKey} key="general" pageSize={pageSize} country="in" category="general"/>}/>
+          <Route exact path="/health"element={<News setProgress={setProgress}  apiKey ={apiKey} key="health" pageSize={pageSize} country="in" category="health"/>}/>
+          <Route exact path="/science"element={<News setProgress={setProgress}  apiKey ={apiKey} key="science" pageSize={pageSize} country="in" category="science"/>}/> 
+          <Route exact path="/sports" element={<News setProgress={setProgress}  apiKey ={apiKey} key="sports" pageSize={pageSize} country="in" category="sports"/>}/>
+          <Route exact path="/technology" element={<News setProgress={setProgress}  apiKey ={apiKey} key="technology" pageSize={pageSize} country="in" category="technology"/>}/>
+          </Routes>
+        </Router>
+      </div>
+    );
+  
+}
+export default App
